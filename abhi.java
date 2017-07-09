8cefd3115b7af30fd3f5333cb235fbab3f151cb3
abhil123456789 <abhilashakumari_pankaj@srmuniv.edu.in>
2017-07-09 12:41:47 +0530

    Add files via upload

---
+++ abhi.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+		Scanner scan = new Scanner(System.in);
+		
+	 int n = scan.nextInt();
+		if (n>0)
+		{
+			System.out.println(n+"n is positive");
+		 }
+		 else if(n<0)
+		 {
+		  System.out.println (n+"n is negative");
+		 }
+		 else
+		  {
+		  	System.out.println(n+"n is zero");
+		  }
+		  }
+} 
+		
+		
+	
