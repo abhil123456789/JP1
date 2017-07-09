bd59268221640a15969827e63a109c4e4ad36cee
abhil123456789 <abhilashakumari_pankaj@srmuniv.edu.in>
2017-07-09 14:34:23 +0530

    Add files via upload

---
+++ swe.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		if (n%2 == 0)
+		{
+			System.out.println(" n is even");
+		 }
+		 else
+		 {
+		 	System.out.println("n is odd");
+		 
+		 }
+		}
+		
+	}
